439dd9761086c6ca11d4a16ac1f7ed07a0189c29
Soufiane Hamoumi <soufiane.hamoumi@student.ehb.be>
2024-12-11 11:38:34 +0100

    Create Colors.ts

---
+++ Student-Meet/constants/Colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+    primary: '#00b894', // Groen voor de knoppen
+    secondary: '#0984e3', // Blauw voor de knoppen
+    background: '#ffffff', // Witte achtergrond
+    inputBorder: '#dfe6e9', // Lichtgrijze rand voor de invoervelden
+    inputBackground: '#f1f2f6', // Lichtgrijze achtergrond voor invoervelden
+    placeholder: '#b2bec3', // Placeholder-kleur
+    text: '#2d3436', // Tekstkleur
+  };
+  
+  export default Colors;
