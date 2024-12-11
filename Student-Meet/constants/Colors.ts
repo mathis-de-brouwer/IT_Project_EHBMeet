bffd08ecd84ed17926cb6510ed4f7c2dc9a02685
Soufiane Hamoumi <soufiane.hamoumi@student.ehb.be>
2024-12-11 11:04:26 +0100

    login and register

--- Student-Meet/constants/Colors.ts
+++ Student-Meet/constants/Colors.ts
@@ -1,19 +1,15 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+
+
+const Colors = {
+  primary: '#00b894', // Groen voor de knoppen
+  secondary: '#0984e3', // Blauw voor de knoppen
+  background: '#ffffff', // Witte achtergrond
+  inputBorder: '#dfe6e9', // Lichtgrijze rand voor de invoervelden
+  inputBackground: '#f1f2f6', // Lichtgrijze achtergrond voor invoervelden
+  placeholder: '#b2bec3', // Placeholder-kleur
+  text: '#2d3436', // Tekstkleur
 };
+export default Colors;
