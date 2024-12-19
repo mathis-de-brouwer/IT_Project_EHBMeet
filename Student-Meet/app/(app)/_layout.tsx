d80d1e1b5297c7d332a7f3ec691c7487de6063e6
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2024-12-19 11:20:49 +0100

    ff

--- Student-Meet/app/(app)/_layout.tsx
+++ Student-Meet/app/(app)/_layout.tsx
@@ -2,19 +2,6 @@ import { Stack } from 'expo-router';
 
 export default function AppLayout() {
   return (
-<<<<<<< HEAD
-    <Stack 
-      screenOptions={{ 
-        headerShown: false,
-        // Prevent going back to auth screens
-        gestureEnabled: false,
-        // Add animation
-        animation: 'slide_from_right',
-      }} 
-    />
-  );
-} 
-=======
     <Stack
       screenOptions={{
         headerShown: false, // Verberg de standaard header voor alle pagina's
@@ -22,4 +9,3 @@ export default function AppLayout() {
     />
   );
 }
->>>>>>> parent of 7aecca93 (Merge branch 'main' into Frond_end)
